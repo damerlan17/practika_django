b1716d7f41b5db98cc666bb45b0776095193eb2a
damerlan17 <uchunakdamerlan@gmail.com>
2025-10-28 17:43:35 +0700

    Админ панель конец 4 части

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -5,24 +5,34 @@ from .models import Book, Genre, Author, BookInstance
 # admin.site.register(Book),
 # admin.site.register(Author),
 # admin.site.register(BookInstance),
-
 admin.site.register(Genre),
 
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
 
 
 admin.site.register(Author, AuthorAdmin)
 
 
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book','imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
-
-
-@admin.register(BookInstance)
-class BookInstanceAdmin(admin.ModelAdmin):
-    pass
-
-
+    inlines = [BooksInstanceInline]
